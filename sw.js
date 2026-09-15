4ee86acbcab5ca9b4915bfe9d21649afab95fc54
manucaama93 <manucaama93@gmail.com>
2026-09-15 18:15:55 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,6 +17,22 @@ self.addEventListener('install', (e) => {
       return cache.addAll(ASSETS);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
